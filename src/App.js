2791c81a6cb7e3327df655dd5b5a460dc20ac1f1
Melissa Astbury <melissajastbury@gmail.com>
2020-04-04 22:18:42 +0100

    started my header container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
-import "./App.css";
 
-function App() {
+import "./App.scss";
+import Header from "./components/Header/Header";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <div className="game__container">
+        <p>game screen</p>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Pairs!</h1>
+        <p>Start Again?</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
